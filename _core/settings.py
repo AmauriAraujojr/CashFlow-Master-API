9660404a1628e23150c9141a1fee8711b5e8f2c7
Amauri Araujo <amauriaraujojr@yahoo.com.br>
2023-09-04 17:44:49 -0300

    feat: add cors

--- _core/settings.py
+++ _core/settings.py
@@ -49,9 +49,11 @@ INSTALLED_APPS = [
     "despesas",
     "drf_spectacular",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware"
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -147,3 +149,6 @@ SPECTACULAR_SETTINGS ={
     "DESCRIPTION":"API para controle de caixa."
 
 }
+CORS_ALLOWED_ORIGINS = [
+	'http://localhost:8000'
+]
